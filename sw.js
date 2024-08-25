e19e0e0f89e29ebc391dc2a112aeb6c7b01b9081
Steven Beshensky <s.beshensky@gmail.com>
2024-08-25 12:16:23 -0500

    register service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-console.log('No service worker in dev mode.');
+const env = location.origin.includes('localhost') ? 'dev' : 'prod';
+if (env === 'prod') {
+  navigator.serviceWorker.register('sw.js');
+} else {
+  console.log('No service worker in dev.');
+}
