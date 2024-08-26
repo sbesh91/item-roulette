078962473a448dea6eaa0c838c756f470c95c876
Steven Beshensky <s.beshensky@gmail.com>
2024-08-25 22:35:57 -0500

    update sw toast to infinite duration

--- sw.ts
+++ sw.ts
@@ -13,10 +13,21 @@ if (env === 'prod') {
             label: 'Update',
             onClick: () => (location.href = '/item-roulette'),
           },
+          duration: Infinity,
         });
       }, 2000);
     });
   });
 } else {
   console.log('No service worker in dev.');
+  setTimeout(() => {
+    toast('Update Available', {
+      description: 'An update has been downloaded and is ready to be used.',
+      action: {
+        label: 'Update',
+        onClick: () => (location.href = '/item-roulette'),
+      },
+      duration: Infinity,
+    });
+  }, 2000);
 }
